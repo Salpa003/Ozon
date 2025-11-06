57a9b214d19f4027d2c3bc82cf175731932d8cef
Салпагаров Руслан <rusik12346rusik@gmail.com>
2025-11-06 11:01:13 +0300

    init 2

---
+++ src/main/java/servlet/HomeServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (Writer writer = resp.getWriter() ) {
+            writer.write("Hello from servlet");
+        }
+    }
+}
